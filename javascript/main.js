ec99c81d8a484d7a2119e45675db73ed209e7bfc
Shreyesh <imshreyesh17@gmail.com>
2019-06-04 22:35:03 +0300

    Information Tabs

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+function showGrid1() {
+    var path = document.getElementById("grid-1-info");
+    if (path.style.display === "none") {
+        path.style.display = "block";
+    } else {
+        path.style.display = "none";
+    }
+}
+
+function showGrid2() {
+    var path = document.getElementById("grid-2-info");
+    if (path.style.display === "none") {
+        path.style.display = "block";
+    } else {
+        path.style.display = "none";
+    }
+}
+
+function showGrid3() {
+    var path = document.getElementById("grid-3-info");
+    if (path.style.display === "none") {
+        path.style.display = "block";
+    } else {
+        path.style.display = "none";
+    }
+}
