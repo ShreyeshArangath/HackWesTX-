a868cacf2d873dcfcbd91ccdb96a2417fa432bc5
Shreyesh <imshreyesh17@gmail.com>
2019-06-05 22:25:59 +0300

    Contact Form Updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,23 @@
 {
+    $(document).ready(function() {
+        $(".submit").click(function(event) {
+            console.log("clicked")
+
+            var email = $(".email").val();
+            var message = $(".message").val();
+            var errorMessage = $(".error");
+            errorMessage.empty();
+
+            if (email.length > 5 && email.includes("@") && email.includes(".") && message.length >= 10) {
+                console.log("Email is valid");
+            } else {
+                errorMessage.append("<h7> Not a valid email address </h7>");
+                event.preventDefault();
+            }
+
+        });
+    })
+
     const gridItems = document.querySelectorAll('.grid');
     const gridContent = document.querySelectorAll(".grid-info");
     const landingPageButton = document.querySelector('.btn-more');
@@ -11,11 +30,21 @@
         this.classList.add("grid-underline");
         const gridContentItem = document.getElementById(`${this.id}-info`);
         gridContentItem.classList.add('show');
+        window.scrollBy({
+            top: 500,
+            left: 100,
+            behavior: 'smooth'
+        });
     }
 
     function removeBorder() {
         gridItems.forEach(item => {
             item.classList.remove('grid-underline')
+            window.scrollBy({
+                bottom: 500,
+                left: 100,
+                behavior: 'smooth'
+            });
         })
     };
     //Listen for tab click 
@@ -37,5 +66,8 @@
         });
     }
     landingPageButton.addEventListener('click', showMore);
+
+
+
 
 }
